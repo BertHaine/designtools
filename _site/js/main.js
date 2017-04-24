60bc5092afdd2d5586f04580e5bd4334e5e44348
BertHaine <bertram.haine@gmail.com>
2017-04-24 14:14:49 -0700

    deactivated fullpage.js for now, changed styling

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,12 +1,23 @@
+
+
+
+
+
 $('#fullpage').fullpage({
   anchors: ['page1', 'page2', 'page3', 'page4'],
-  sectionsColor: ['#FFF', 'orange', '#C0C0C0', '#ADD8E6'],
+  // sectionsColor: ['#FFF', '#F68383', '#f8c881', '#f7fa7c', '#D2FC74', '#71FC7B', '#72FDDA', '#70D1FC', '#D2FC74', '#70D1FC','#7570FB','#D871FB'],
   scrollingSpeed: 900,
   csss3: true,
   verticalCentered: false,
-  fadingEffect: true
+  fadingEffect: true,
+  allowPageScroll: true,
+   paddingBottom: '0',
+   resize: false,
+   scrollOverflow: false,
+   touchSensitivity: 10
 
 });
+
 
 var trigger = document.getElementById("trigger");
 var trigger2 = document.getElementById("trigger2");
@@ -14,7 +25,7 @@ var trigger2 = document.getElementById("trigger2");
 window.addEventListener("scroll", function(){
   containers = document.getElementsByClassName('container');
   Array.prototype.forEach.call(containers, function(container){
-    if(container.offsetTop < window.pageYOffset + 100 && container.offsetTop > window.pageYOffset - 100){
+    if(container.offsetTop < window.pageYOffset + 0 && container.offsetTop > window.pageYOffset - 0){
       console.log(container.dataset.background);
       document.querySelector('body').style.background = container.dataset.background;
     }
